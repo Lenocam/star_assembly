e85f8eddaa2953cc54b80700528a5e5135dbb9d0
vagrant <vagrant@devbox>
2014-03-28 18:05:22 +0000

    added application.html.erb to application whilst being able to keep bootstrap formating

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ StarAssembly::Application.routes.draw do
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Forem relies on it being the default of "forem"
-  mount Forem::Engine, :at => '/'
+  mount Forem::Engine, :at => "/"
+  root :to => "forem/forums#index"
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
